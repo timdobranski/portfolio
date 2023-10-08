8dd0321f464c38ca18f8c9433e64e9347a191ed3
Tim <timdobranski@gmail.com>
2023-10-07 22:34:16 -0700

    Added google font and tentative background img

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,13 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import background from '../../public/images/portfolio-background.svg';
+import Image from 'next/image';
+import { Josefin_Sans } from '@next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+
+const josefin = Josefin_Sans({
+  subsets: ['latin'],
+  weight: ['100']
+});
 
 export const metadata = {
   title: 'Tim Dobranski',
@@ -11,7 +17,29 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body id='app' className={josefin.className}>
+        <Image src={background} alt='Tim Dobranski' className='background' />
+        <div id='header-container'>
+          <h1 className='title '>TIM DOBRANSKI</h1>
+          <p className='description '>Web developer, musician, educator, and dog dad to Aria</p>
+          <div className='grid '>
+            <a href='' className='card'>
+              <h2>PROJECTS</h2>
+            </a>
+            <a href='' className='card'>
+              <h2>RESUME</h2>
+            </a>
+            <a href='' className='card'>
+              <h2>ABOUT ME</h2>
+            </a>
+            <a href='' className='card'>
+              <h2>CONNECT</h2>
+            </a>
+          </div>
+
+        </div>
+        {children}
+      </body>
     </html>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,35 +1,12 @@
-import styles from './page.module.css'
+import styles from './Home.module.css'
+
 import me from '../../public/images/me.jpg'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1 className={styles.title}>Tim Dobranski</h1>
-      <p className={styles.description}>Web developer, musician, educator, and dog dad to Aria</p>
-
-      <div className={styles.grid}>
-        <a href='' className={styles.card}>
-          <h2>Projects &rarr;</h2>
-          <p>See my work</p>
-        </a>
-        <a href='' className={styles.card}>
-          <h2>Resume &rarr;</h2>
-          <p>See my work history</p>
-        </a>
-        <a href='' className={styles.card}>
-          <h2>Blog &rarr;</h2>
-          <p>Read my thoughts</p>
-        </a>
-        <a href='' className={styles.card}>
-          <h2>Connect &rarr;</h2>
-          <p>Get in touch</p>
-        </a>
-      </div>
-
-      <div className={styles.grid}>
-        <Image src={me} className={styles.me} alt="Tim Dobranski" />
-      </div>
+    <main id='home'>
+      {/* <Image src={me} alt='Tim Dobranski' className={styles.me} /> */}
 
     </main>
   )
