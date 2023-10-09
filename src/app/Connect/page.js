ab9b72889dbc6ea96d3d41874f87a85908f9a979
Tim <timdobranski@gmail.com>
2023-10-09 12:00:37 -0700

    Preliminary contact page complete

--- src/app/Connect/page.js
+++ src/app/Connect/page.js
@@ -3,10 +3,25 @@ import styles from './Connect.module.css'
 export default function Connect() {
   return (
     <div className={styles.connectContainer}>
-      <div className={styles.oversizedYellowRing}>
+
         <h1>CONNECT</h1>
-        <p>Feel free to reach out to me at</p>
+        <p>{`I'm always happy to make a new connection.`}</p>
+        <p>{`Feel free to reach out in the following ways:`}</p>
+        <div className={styles.contactInfoContainer}>
+          <div className={styles.contactHeaders}>
+            <p>Email</p>
+            <p>Phone</p>
+            <p>LinkedIn</p>
+            <p>Github</p>
+        </div>
+        <div className={styles.contactInfo}>
+            <p>timdobranski@gmail.com</p>
+            <p>{'(619) 820-6213'}</p>
+            <p>linkedin.com/in/timdobranski</p>
+            <p>github.com/timdobranski</p>
+        </div>
       </div>
     </div>
   )
 }
+
