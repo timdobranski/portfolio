5dc5756cb56b26ec5378b127b01b5a1ef6c8bb82
Tim <timdobranski@gmail.com>
2023-10-08 21:53:26 -0700

    Ring animations working l-r but not r-l yet

--- src/app/page.js
+++ src/app/page.js
@@ -6,89 +6,70 @@ import me from '../../public/images/Me-2.jpg'
 import { useState } from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+
 
 export default function Home() {
-  const [ activeRing, setActiveRing ] = useState(0)
+  const [ activeRing, setActiveRing ] = useState(1)
 
   const rings = [
-    {
-      header: 'WELCOME!',
-      text: 'My name is Tim, and I develop websites and apps',
-      color: 'greenGlow'
-    },
-    {
-      header: 'ABOUT ME',
-      text: 'To learn more about me and my background, click on the about me tab above',
-      color: 'blueGlow'
-    },
     {
       header: 'CONNECT',
       text: 'To connect with me, click on the connect tab above',
       color: 'yellowGlow'
     },
     {
+      header: 'WELCOME!',
+      text: 'My name is Tim, and I develop websites and apps',
+      color: 'greenGlow'
+    },
+    {
       header: 'MY WORK',
       text: 'To see more of my work, click on the projects tab above',
       color: 'redGlow'
+    },
+    {
+      header: 'ABOUT ME',
+      text: 'To learn more about me and my background, click on the about me tab above',
+      color: 'blueGlow'
     },
   ]
 
 
   const handleNextRing = () => {
-    if (activeRing === 3) {
-      setActiveRing(0)
-    } else {
-      setActiveRing(activeRing + 1)
-    }
-  }
-  const handlePreviousRing = () => {
-    if (activeRing === 0) {
-      setActiveRing(3)
-    } else {
-      setActiveRing(activeRing - 1)
-    }
-  }
+    setActiveRing((prevRing) => (prevRing + 1) % rings.length);
+}
+
+const handlePreviousRing = () => {
+    setActiveRing((prevRing) => (prevRing - 1 + rings.length) % rings.length);
+}
 
 
   return (
     <main id={styles.home}>
-        <FontAwesomeIcon icon={faChevronLeft} className={styles.leftNav} onClick={handlePreviousRing}/>
-        <FontAwesomeIcon icon={faChevronRight} className={styles.rightNav} onClick={handleNextRing} />
+    <FontAwesomeIcon icon={faChevronLeft} className={styles.leftNav} onClick={handlePreviousRing}/>
+    <FontAwesomeIcon icon={faChevronRight} className={styles.rightNav} onClick={handleNextRing} />
 
-        {/* Previous Ring */}
-        {(() => {
-            const i = (activeRing - 1 + rings.length) % rings.length;
-            const ring = rings[i];
-            return (
-                <div className={`${styles.ring} ${styles[ring.color]} ${styles.inactiveLeft}`} key={i}>
-                    <h1>{ring.header}</h1>
-                    <p>{ring.text}</p>
-                </div>
-            );
-        })()}
+    {rings.map((ring, index) => {
+        let ringClass;
+        if (index === activeRing) {
+            ringClass = styles.activeRing;
+        } else if (index === (activeRing + 1) % rings.length) {
+            ringClass = styles.inactiveRight;
+        } else if (index === (activeRing - 1 + rings.length) % rings.length) {
+            ringClass = styles.inactiveLeft;
+        } else {
+            ringClass = styles.hidden; // For rings that are not in the immediate view
+        }
 
-        {/* Active Ring */}
-        {(() => {
-            const ring = rings[activeRing];
-            return (
-                <div className={`${styles.ring} ${styles[ring.color]} ${styles.activeRing}`} key={activeRing}>
-                    <h1 className={styles.ringHeader} >{ring.header} </h1>
-                    <p className={styles.ringText} >{ring.text}</p>
-                </div>
-            );
-        })()}
-
-        {/* Next Ring */}
-        {(() => {
-            const i = (activeRing + 1) % rings.length;
-            const ring = rings[i];
-            return (
-                <div className={`${styles.ring} ${styles[ring.color]} ${styles.inactiveRight}`} key={i}>
-                    <h1>{ring.header}</h1>
-                    <p>{ring.text}</p>
-                </div>
-            );
-        })()}
-    </main>
+        return (
+            <div className={`${styles.ring} ${styles[ring.color]} ${ringClass}`} key={index}>
+                <h1 className={styles.ringHeader}>{ring.header}</h1>
+                <p className={styles.ringText}>{ring.text}</p>
+            </div>
+        );
+    })}
+</main>
 )
 }
