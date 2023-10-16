c4af0c681ea2e744b10e9f4adb7b03e9c54221e6
Tim <timdobranski@gmail.com>
2023-10-15 18:46:43 -0700

    Center-aligned Projects thumbs

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -4,16 +4,22 @@ import Image from 'next/image';
 import styles from './Gallery.module.css'
 import { useState, useEffect } from 'react';
 
-
 export default function Gallery({ images }) {
   const [mainImage, setMainImage] = useState(images[0]);
+  const [isModalOpen, setModalOpen] = useState(false); // New state for modal
+
+  const closeModal = () => setModalOpen(false);
+
+  const openModal = () => setModalOpen(true);
 
   return (
     <div className={styles.gallery}>
-      <div className={styles.mainImageContainer}>
+      {/* Main Image */}
+      <div className={styles.mainImageContainer} onClick={openModal}> {/* Open modal when main image is clicked */}
         <Image src={mainImage} alt="Main Image" layout="fill" objectFit="contain"/>
       </div>
 
+      {/* Thumbnails */}
       <div className={`${styles.thumbnails}`}>
         {images.map((image, index) => (
           <div key={index} className={styles.thumbnailContainer} onClick={() => setMainImage(image)}>
@@ -21,6 +27,15 @@ export default function Gallery({ images }) {
           </div>
         ))}
       </div>
+
+      {/* Modal */}
+      {isModalOpen && (
+        <div className={styles.modalOverlay} onClick={closeModal}>
+          <div className={styles.modal}>
+            <Image src={mainImage} alt="Modal Main Image" layout="responsive" width={800} height={800} />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
