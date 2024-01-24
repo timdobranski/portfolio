5f9d94aa4301c4a28faf128ad32aac72abedd04b
Tim <timdobranski@gmail.com>
2024-01-24 12:31:14 -0800

    Updated Parkway Periodical Projcet page

--- src/app/Rings/Projects/parkway-periodical/page.js
+++ src/app/Rings/Projects/parkway-periodical/page.js
@@ -7,17 +7,13 @@ export default function ParkwayPeriodical() {
   const projectData = {
     name: 'PARKWAY PERIODICAL',
     date: 'Fall 2023',
-    description: [`This was a unique learning experience with new technical challenges that really helped me grow as a developer.`,
-    `The school administration wanted an updated, more visually appealing blog. The existing blog was created using Google Sites.
-    The ideal scenario for them was to continue using Google Sites to add new content. So I set to work designing a scraper and a
-    caching tool to extract the content from the old blog and transform it into my new blog with a different visual container.
-     This allowed school administrators to continue building the site in their preferred format, while enabling a visual overhaul
-     and content reordered to multiple pages. The disadvantage of this approach was that if Google Sites should update their
-     implementation there is the potential for the data fetcher to break. I handled this scenario by redirecting the user to the
-     original blog if there should be an error in the data fetching process.`,
-    `The main challenges for this project were to extract styling data, both the stylesheet and inline styling, and also to extract
-     images in a way that they would persist despite no active session on Google Sites.`],
-    tech: ['Next.js'],
+    description: [`This project has been a unique learning experience for me. It's a redesign of the Parkway Academy school blog,
+    which was originall built with Google Sites. I created a Next.js app that features the original blog postings format, as well as
+    more robust features like an archive page and an About page which introduces the school administrators. The majority of the work
+    went into creating the post editor back end for the school administrators, which allows them to create, edit and delete posts.
+    I knew there were easier ways to tie into APIs for this but I wanted to learn how to do it from scratch. As of Jan 2024, the
+    project is nearly complete. School administrators are very happy with the early results and I'm looking forward to finishing it up.`],
+    tech: ['Next.js', 'Postgres', 'Node.js'],
     images: [parkwayPeriodical],
     deployLink: {url:'https://parkway-academy-periodical.vercel.app/', text: 'See It In Action'},
     repoLink: {url:'https://github.com/timdobranski/parkway-periodical.git', text: 'View Repo On Github'}
