cf39e2a7d2745a9ebb07a70f3c3862c83b1b7628
Tim <timdobranski@gmail.com>
2024-01-24 13:11:59 -0800

    Added more recent screenshots for Parkway Periodical

--- src/app/Rings/Projects/parkway-periodical/page.js
+++ src/app/Rings/Projects/parkway-periodical/page.js
@@ -1,6 +1,8 @@
 import Project from '../../../../components/Project/Project';
 import parkwayPeriodical from '../../../../../public/images/projects/parkway-periodical/parkway-periodical.png';
-
+import photo1 from '../../../../../public/images/projects/parkway-periodical/1.png';
+import photo2 from '../../../../../public/images/projects/parkway-periodical/2.png';
+import photo3 from '../../../../../public/images/projects/parkway-periodical/3.png';
 
 export default function ParkwayPeriodical() {
 
@@ -14,7 +16,7 @@ export default function ParkwayPeriodical() {
     I knew there were easier ways to tie into APIs for this but I wanted to learn how to do it from scratch. As of Jan 2024, the
     project is nearly complete. School administrators are very happy with the early results and I'm looking forward to finishing it up.`],
     tech: ['Next.js', 'Postgres', 'Node.js'],
-    images: [parkwayPeriodical],
+    images: [parkwayPeriodical, photo1, photo2, photo3],
     deployLink: {url:'https://parkway-academy-periodical.vercel.app/', text: 'See It In Action'},
     repoLink: {url:'https://github.com/timdobranski/parkway-periodical.git', text: 'View Repo On Github'}
   }
