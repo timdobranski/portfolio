0a7fa5be33e2a1e4fcca007c24d7ba449e31ec04
Tim <timdobranski@gmail.com>
2024-01-25 02:05:11 -0800

    Fixed typo in parkway description

--- src/app/Rings/Projects/parkway-periodical/page.js
+++ src/app/Rings/Projects/parkway-periodical/page.js
@@ -8,9 +8,9 @@ export default function ParkwayPeriodical() {
 
   const projectData = {
     name: 'PARKWAY PERIODICAL',
-    date: 'Fall 2023',
+    date: 'Winter 2023',
     description: [`This project has been a unique learning experience for me. It's a redesign of the Parkway Academy school blog,
-    which was originall built with Google Sites. I created a Next.js app that features the original blog postings format, as well as
+    which was originally built with Google Sites. I created a Next.js app that features the original blog postings format, as well as
     more robust features like an archive page and an About page which introduces the school administrators. The majority of the work
     went into creating the post editor back end for the school administrators, which allows them to create, edit and delete posts.
     I knew there were easier ways to tie into APIs for this but I wanted to learn how to do it from scratch. As of Jan 2024, the
